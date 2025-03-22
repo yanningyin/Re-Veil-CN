869f6e6c038464299c3a06a43ec06592381f2409
Yanning Yin <yanning@Yannings-MacBook-Air.local>
2025-03-22 21:15:51 +0100

    fixed backhend upload issues

--- multilingual_motr/zh/src/magpie.config.js
+++ multilingual_motr/zh/src/magpie.config.js
@@ -1,7 +1,7 @@
 export default {
   experimentId: '1',
-  serverUrl: 'https://re-veil-project.herokuapp.com/',
-  socketUrl: 'wss://re-veil-project.herokuapp.com/socket',
+  serverUrl: 'https://re-veil-backend-83c42dec8100.herokuapp.com/',
+  socketUrl: 'wss://re-veil-backend-83c42dec8100.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.co/submissions/complete?cc=C1I4D2OB',
   contactEmail: 'cui.ding@uzh.ch',
