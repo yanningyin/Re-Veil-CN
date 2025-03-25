255a247ecb48b4e226ed2b0d69425ac0820874d9
cuierd <cui.ding@uzh.ch>
2025-03-25 13:53:40 +0100

    ZH adapation

--- multilingual_motr/zh/src/magpie.config.js
+++ multilingual_motr/zh/src/magpie.config.js
@@ -2,13 +2,10 @@ export default {
   experimentId: '67',
   serverUrl: 'https://re-veil-backend-83c42dec8100.herokuapp.com/',
   socketUrl: 'wss://re-veil-backend-83c42dec8100.herokuapp.com/socket',
-  // experimentId: '34',
-  // serverUrl: 'https://re-veil-backend-83c42dec8100.herokuapp.com/',
-  // socketUrl: 'wss://re-veil-backend-83c42dec8100.herokuapp.com/socket',
   // this will be used in prolific mode
   completionUrl: 'https://app.prolific.com/submissions/complete?cc=CGYULV9Z',
   contactEmail: 'cui.ding@uzh.ch',
   // Either 'debug', 'directLink' or 'prolific'
   mode: 'prolific',
-  language: 'en'
+  language: 'zh'
 };
