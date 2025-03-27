9a45d7c09def9ae2a8799d5d5710c19ee85d909a
Yanning <Yanning@pc-sw016.chemie.unibas.ch>
2025-03-27 14:58:41 +0100

    added a font for chinese

---
+++ multilingual_motr/zh-test/vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.(csv|tsv)$/,
+          loader: 'csv-loader',
+          options: {
+            dynamicTyping: true,
+            header: true,
+            skipEmptyLines: true
+          }
+        }
+      ]
+    }
+  },
+  lintOnSave: false,
+  pluginOptions: {
+    lintStyleOnBuild: false,
+    stylelint: {}
+  },
+  	publicPath:
+    process.env.NODE_ENV === 'production'
+    ? '/Re-Veil-CN/multilingual_motr/zh-test/'
+    : '/'
+};
